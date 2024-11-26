19976f14bba8c2d5d5dc15f618d1fd0f03758a82
mle-flem <mle-flem@student.42.fr>
2024-11-26 15:25:15 +0100

    feat: add a macro to automatically open/close files

--- include/utils.h
+++ include/utils.h
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:04:06 by mle-flem          #+#    #+#             */
-/*   Updated: 2024/11/26 14:57:28 by mle-flem         ###   ########.fr       */
+/*   Updated: 2024/11/26 15:24:14 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,24 @@
 # define CLR_OK "\x1b[0;1;92m"
 # define CLR_KO "\x1b[0;1;91m"
 
-# define TEST(title_str, code) { title(title_str); {code}; printf("\n"); }
+# define TEST(title_str, code) { \
+	title(title_str ":"); \
+	{code}; \
+	printf(" \n");\
+}
+
+# define TESTF(title_str, code) { \
+	title(title_str ":"); \
+	int fd = open("testdata/" title_str, O_RDONLY); \
+	if (fd < 0) \
+		printf("%s [CANNOT OPEN %s]%s", CLR_KO, "testdata/" title_str, CLR_RESET); \
+	else \
+	{ \
+		{code}; \
+		close(fd); \
+	} \
+	printf(" \n");\
+}
 
 void	handle_segv(int sig);
 void	check(bool ok);
--- tests/mandatory.c
+++ tests/mandatory.c
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:05:00 by mle-flem          #+#    #+#             */
-/*   Updated: 2024/11/26 15:11:10 by mle-flem         ###   ########.fr       */
+/*   Updated: 2024/11/26 15:24:25 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(void)
 
 	signal(SIGSEGV, handle_segv);
 
-	TEST("Invalid fd:", {
+	TEST("Invalid fd", {
 		/* 1 */ gnl_check(1000, NULL);
 		/* 2 */ gnl_check(-1, NULL);
 		fd = open("testdata/alt_no_nl", O_RDONLY);
@@ -32,28 +32,22 @@ int	main(void)
 		/* 3 */ gnl_check(fd, NULL);
 	});
 
-	TEST("empty:", {
-		fd = open("testdata/empty", O_RDONLY);
+	TESTF("empty", {
 		/* 1 */ gnl_check(fd, NULL);
 		/* 2 */ gnl_check(fd, NULL);
-		close(fd);
 	});
 
-	TEST("nl:", {
-		fd = open("testdata/nl", O_RDONLY);
+	TESTF("nl", {
 		/* 1 */ gnl_check(fd, "\n");
 		/* 2 */ gnl_check(fd, NULL);
-		close(fd);
 	});
 
-	TEST("nl_x5:", {
-		fd = open("testdata/nl_x5", O_RDONLY);
+	TESTF("nl_x5", {
 		/* 1 */ gnl_check(fd, "\n");
 		/* 2 */ gnl_check(fd, "\n");
 		/* 3 */ gnl_check(fd, "\n");
 		/* 4 */ gnl_check(fd, "\n");
 		/* 5 */ gnl_check(fd, "\n");
 		/* 6 */ gnl_check(fd, NULL);
-		close(fd);
 	});
 }
